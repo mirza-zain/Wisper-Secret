1d93831519b1cc1bfa74cc612d172c1654a7f5b2
Mirza Zain <hellotoworld15@gmail.com>
2023-08-24 13:00:10 +0500

    backend added test 1

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+    try {
+        const response = await axios.post("https://secrets-api.appbrewery.com/secret");
+        const result = response.data;
+        res.render("index.ejs", { secret: result[Math.floor(Math.random()*result.length)]});
+    }
+    catch (error) {
+        console.log("Failed to make request there are two possible soluition for it either you have send more then 100 requests or you are not connected to internet",error.message);
+        res.render("index.ejs", {error : error.message});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
