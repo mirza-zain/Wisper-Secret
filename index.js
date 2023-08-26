fd62be3cb8fc224d17b47c3dece71bf6f8d10143
Mirza Zain <hellotoworld15@gmail.com>
2023-08-26 13:39:17 +0500

    Test 2

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
-});
-
-app.post("/submit", async (req, res) => {
     try {
         const response = await axios.post("https://secrets-api.appbrewery.com/random");
         const result = response.data;
-        res.render("index.ejs", { secret: result[Math.floor(Math.random()*result.length)],});
+        console.log(result);
+        res.render("index.ejs", { secret: result[Math.floor(Math.random()*result.length)]});
     }
     catch (error) {
         console.log("Failed to make request there are two possible soluition for it either you have send more then 100 requests or you are not connected to internet",error.message);
